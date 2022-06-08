8cb966b79f63ca45f4ff7be5018c901ac4562553
OliverAtondo <66600337+OliverAtondo@users.noreply.github.com>
2022-06-07 20:35:04 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,25 @@
+//NO HACE FALTA MODIFICAR ESTE CÓDIGO.
+
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 const io = require('socket.io')(server);
+// Puras librerías y la constante io para el server.
 
-app.use(express.static('public'));
+app.use(express.static('public')); // Usamos la carpeta public para mostrar la página WEB.
 
-var usuarios = [];
-
-io.on('connection', function (socket) {
+io.on('connection', function (socket) { // Verifica si se conecto alguien.
   console.log('Nueva Dispositivo Conectado');
-  socket.on('telemetria', function(data)
+  socket.on('telemetria', function(data) // Escucha los clientes que manden un JSON con la orden 'telemetria'
   {
-    console.log(data);
-    io.emit('telemetria', data);
+    console.log(data); // Imprime lo que le manden para reconocer lo que se manda.
+    io.emit('telemetria', data);// Se le manda a todos los clientes un JSON 'telemetria'.
   });
 
-  socket.on('controlar_ESP', function(data)
+  socket.on('controlar_ESP', function(data) // Aquí se mandan las órdenes al ESP32 que este conectado al server.
   {
-    console.log(data);
-    io.emit('controlar_ESP', data);
+    console.log(data); // Se imprime la órden recibida.
+    io.emit('controlar_ESP', data); // Manda al ESP32 una órden en formato JSON.
   });
 
 });
